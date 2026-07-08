8de4ed7e2fd86c1fe4551a37eaf6b550649bc09e
neon-lia <liabarrett94@gmail.com>
2026-07-08 18:51:08 -0500

    DB check

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+window.addEventListener('load', (event) => {
+    import { createClient } from '@supabase/supabase-js'
+    const supabaseUrl = 'https://oaknbxjzuvjlwpuggcji.supabase.co'
+    const supabaseKey = sb_publishable_HfOAPoqY387Nkffk993Ccg_IiuXPtyH
+    const supabase = createClient(supabaseUrl, supabaseKey)
+});
+
+
 function loadData() {
     return "Hello";
+    let { data: Food, error } = await supabase
+        .from('Food')
+        .select('Name')
 }
+
